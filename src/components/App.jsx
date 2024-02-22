be9c431762cfa38a9e5567c8b904a32b8fb13b3f
Levina Tani <edelaida@gmail.com>
2024-02-22 11:19:24 +0200

    gallery

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,10 @@
+import Button from './Button/Button';
+import ImageGallery from './ImageGallery/ImageGallery';
+import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
+import Loader from './Loader/Loader';
+import Modal from './Modal/Modal';
+import Searchbar from './Searchbar/Searchbar';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +14,16 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
+      <Searchbar />
+      <ImageGallery />
+      <ImageGalleryItem />
+      <Button />
+      <Modal />
+      <Loader />
     </div>
   );
 };
